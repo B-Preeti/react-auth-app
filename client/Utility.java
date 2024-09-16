c9f2ef3fbc5692f193636d949be7c5fa67d8576d
preeti <bindigaripreeti.com>
2024-09-17 01:29:51 +0530

    preetib0001 - Wrote the code for finding the area of the circle

---
+++ client/Utility.java
@@ -0,0 +1,15 @@
+
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    
+    public static double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    
+    public static void preeti() {
+        System.out.println("I am Preeti");
+    }
+}
